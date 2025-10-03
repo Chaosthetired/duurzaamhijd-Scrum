edab3d0a5ecc098b23eb23e1c1bf61198cfaf8f9
Chaosthetired <156306479+Chaosthetired@users.noreply.github.com>
2025-10-03 10:21:14 +0200

    Add files via upload

---
+++ array_example.php
@@ -0,0 +1,12 @@
+<?
+$company = [
+    "company_id"             => 1,                     // serial (int, auto-increment)
+    "company_name"           => "Example Corp",        // varchar
+    "company_type"           => 2,                     // int (e.g., type_id)
+    "company_emessions"      => 1500,                  // int (typo? emissions)
+    "logo_id"                => 12,                    // int
+    "company_source"         => "self-reported",       // varchar
+    "company_electricity_use"=> 32000,                 // int (kWh or similar)
+    "company_active"         => true                   // bool
+];
+?>
